5b459af5003433f55f6965a3728f64bdc9d306f2
Raouf Madani <raoufmadani@iMac-de-Raouf.local>
2020-12-24 23:30:12 +0100

    auth screens

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import BmsNavigation from './navigation/bmsNavigation';
 import {createStore,combineReducers,applyMiddleware} from 'redux';
@@ -8,6 +8,12 @@ import ReduxThunk from "redux-thunk";
 import {Provider} from 'react-redux';
 import {enableScreens} from 'react-native-screens';
 import {AppLoading} from 'expo';
+const fetchFonts = () =>{
+  return Font.loadAsync({
+     'poppins': require('./assets/fonts/Poppins-Regular.ttf'),
+     'poppins-bold': require('./assets/fonts/Poppins-Bold.ttf')
+  });
+};
 
 export default function App() {
 
@@ -17,7 +23,16 @@ export default function App() {
 //   });
 
 // const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+const [fontLoaded, setFontLoaded] = useState(false);
 
+if(!fontLoaded){
+  return(
+    <AppLoading 
+     startAsync={fetchFonts}
+     onFinish={()=> setFontLoaded(true)}
+    />
+  )
+}
   return (
     <BmsNavigation />
   );
